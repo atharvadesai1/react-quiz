987c430b9b07c1c1ad7b70835703f16d8526e394
atharvadesai1 <113234377+atharvadesai1@users.noreply.github.com>
2023-08-21 11:04:03 +0530

    working

---
+++ src/components/Question.js
@@ -0,0 +1,36 @@
+let option_given 
+const quizPart = [
+    {
+        question: '1. What is the maximum length of a Python identifier?',
+        option_given: [65,79,82,71],
+        answer: 1
+    },
+    {
+        question: '2. How many bytes does a float take up in C?',
+        option_given: [2,1,85,4],
+        answer: 3
+    },
+    {
+
+        question: '3. Which method can be used to find the length of a string in java?',
+        option_given: ['getSize()','getLength()','length()','len()'] ,
+        answer: 2
+    },
+    {
+        question: '4. How do you open and read a text file named "data.txt" in Python?',
+        option_given: ['file = open("data.txt", "r")','file = read("data.txt")','file = open("data.txt", "read")','file = readfile("data.txt")'],
+        answer: 0
+    },
+    {
+        question: '5. Which of the following user-defined header file extension used in c++?',
+        option_given: ['hg','cpp','h','hf'],
+        answer: 2
+    }
+]
+
+// console.log(quizPart[0].question)
+
+
+
+export {quizPart};
+
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {quizPart} from './Question'
+
 
 function Quiz(props) {
   return (
@@ -7,9 +9,15 @@ function Quiz(props) {
         <h1 id='heading'>{props.heading}</h1>
         <div className="quiz_box">
             <div className="screen">
+              <h1>{quizPart[0].question}</h1>
             </div>
             <div className="options">
-                
+                <div className="option a"></div>
+                <div className="option b"></div>
+                <div className="option c"></div>
+                <div className="option d"></div>
+                <button>Previous</button>
+                <button>Next</button>
             </div>
         </div>
       </div>
